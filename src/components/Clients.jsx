b9f6b7ca3e81aca7d97094fd1ccdd28a7145d2cf
dimusiq <dumojke@gmail.com>
2022-12-02 19:00:01 +0500

    added Clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { clients } from '../constants';
+import styles from '../styles';
 
-function Clients() {
-	return <div></div>;
-}
+const Clients = () => (
+	<section className={`${styles.flexCenter}`}>
+		<div className={`${styles.flexCenter} flex-wrap w-full`}>
+			{clients.map((client) => {
+				return (
+					<div
+						key={clients.id}
+						className={`flex-1 ${styles.flexCenter} sm:min-w-[192px]`}>
+						<img
+							src={client.logo}
+							alt='client'
+							className='sm:w-[192px] w-[100px]'
+						/>
+					</div>
+				);
+			})}
+		</div>
+	</section>
+);
 
 export default Clients;
